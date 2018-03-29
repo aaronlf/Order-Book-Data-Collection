0c3467b5e35dad18ca639d678fad86242b6cf0c7
Aaron LalorFitzpatrick <aaron.lalorfitzpatrick2@mail.dcu.ie>
2018-03-29 14:03:10 +0100

    Added get_orderbook()

--- Data collection/orderbook_hdf.py
+++ Data collection/orderbook_hdf.py
@@ -7,6 +7,7 @@ Created on Wed Mar 28 15:07:22 2018
 """
 import pandas as pd
 import time
+import numpy as np
 
 def write_to_hdf(symbol,exchange_name,df):
     key = symbol+'_'+exchange_name
@@ -33,3 +34,73 @@ def append_to_hdf(symbol,exchange_name,df):
     store.append(key,df,ignore_index=True)
     store.close()
     pass
+
+
+#------------------------------------------------------------------------------
+    
+
+def get_orderbook(symbol,exch_object):
+    orderbook = exch_object.fetch_l2_order_book(symbol,3)
+    precision = exch_object.markets[symbol]['precision']
+
+
+    timestamp = orderbook['timestamp']    
+    bid_volume = 0
+    bid_weighted_price = 0
+    bids = orderbook['bids'][:3]
+    for i in bids:
+        bid_volume += i[1]
+        bid_weighted_price += i[0]*i[1]
+    bid_weighted_price = bid_weighted_price / bid_volume
+    bid_volume = 0
+    for i in bids:
+        if i[0] >= bid_weighted_price:
+            bid_volume += i[1]
+    
+    ask_volume = 0
+    ask_weighted_price = 0
+    asks = orderbook['asks'][:3]
+    for i in asks:
+        ask_volume += i[1]
+        ask_weighted_price += i[0]*i[1]
+    ask_weighted_price = ask_weighted_price / ask_volume
+    ask_volume = 0
+    for i in asks:
+        if i[0] <= ask_weighted_price:
+            ask_volume += i[1]
+            
+    orderbook_dict = {
+            'timestamp':[timestamp],
+            'bid_price':[round(bid_weighted_price,precision['price'])],
+            'bid_volume':[round(bid_volume,precision['amount'])],
+            'ask_price':[round(ask_weighted_price,precision['price'])],
+            'ask_volume':[round(ask_volume,precision['amount'])]}
+    
+    df = pd.DataFrame(orderbook_dict)
+    orderbook_df = convert_orderbook_dtypes(df,precision)
+    return orderbook_df
+    
+    
+#------------------------------------------------------------------------------
+    
+    
+def convert_orderbook_dtypes(df,precision):
+    if precision['price'] < 7:
+        price_dtype = np.float32
+    else:
+        price_dtype = np.float64
+    if precision['amount'] < 7:
+        volume_dtype = np.float32
+    else:
+        volume_dtype = np.float64
+    new_df = df.astype({
+            'bid_price':price_dtype,
+            'bid_volume':volume_dtype,
+            'ask_price':price_dtype,
+            'ask_volume':volume_dtype})
+    return new_df
+    
+    
+#------------------------------------------------------------------------------    
+
+    
