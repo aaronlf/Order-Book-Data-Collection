038448ff6cd68292d2a83327ff328a3fbcc13d66
Aaron LalorFitzpatrick <aaron.lalorfitzpatrick2@mail.dcu.ie>
2018-04-03 18:04:31 +0100

    Added newline and changed default serverNumber to 0

--- Symbols on exchanges/initialise_exchanges.py
+++ Symbols on exchanges/initialise_exchanges.py
@@ -99,8 +99,8 @@ def main(n):
 
 #------------------------------------------------------------------------------
     
-serverNumber = 3
-print("Initialising...")
+serverNumber = 0
+print("\nInitialising...\n")
 exchanges = main(serverNumber)
 
 #------------------------------------------------------------------------------
